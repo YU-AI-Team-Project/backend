95a2ff7087c80f7aa4b00a4c25e62ce89be3f8f8
KimJiGwang <5244416@naver.com>
2025-05-19 19:20:47 +0900

    feat:Docker 수정, aibackend main.py 수정

--- aibackend/app/main.py
+++ aibackend/app/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 from aibackend.app import models
 from aibackend.app.database import engine
 from aibackend.app.routers import auth, stock_info
@@ -12,3 +14,10 @@ app.include_router(stock_info.router)
 
 #서버 실행 시 DB에 테이블이 없다면 models.py에 있는 정보 토대로 자동생성
 models.Base.metadata.create_all(bind=engine)
+
+app.mount("/static", StaticFiles(directory="frontend/static"), name="static")
+
+# 루트 요청에 React index.html 응답
+@app.get("/")
+def serve_react_app():
+    return FileResponse("frontend/index.html")
