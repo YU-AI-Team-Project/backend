4668cbc1053545b5b8557d94a93cc79f41290340
KimJiGwang <5244416@naver.com>
2025-05-21 14:54:48 +0900

    feat:도커 최신화

--- aibackend/app/main.py
+++ aibackend/app/main.py
@@ -7,6 +7,16 @@ from aibackend.app.routers import auth, stock_info
 
 app = FastAPI()
 
+#--------------------------배포용--------------------------------
+# React 정적 파일 제공
+app.mount("/static", StaticFiles(directory="frontend/static"), name="static")
+
+# 모든 경로 index.html 반환
+@app.get("/{full_path:path}")
+def serve_react_app_catch_all(full_path: str):
+    return FileResponse("frontend/index.html")
+#----------------------------------------------------------
+
 #router폴더 생성해서 기능별 API 관리
 app.include_router(auth.router)
 app.include_router(stock_info.router)
@@ -14,10 +24,3 @@ app.include_router(stock_info.router)
 
 #서버 실행 시 DB에 테이블이 없다면 models.py에 있는 정보 토대로 자동생성
 models.Base.metadata.create_all(bind=engine)
-
-app.mount("/static", StaticFiles(directory="frontend/static"), name="static")
-
-# 루트 요청에 React index.html 응답
-@app.get("/")
-def serve_react_app():
-    return FileResponse("frontend/index.html")
